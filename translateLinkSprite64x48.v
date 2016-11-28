b3f6aa72d180a22fafb6d2f6ab4ea1cb070330c2
phosphores <paulyf.wang@mail.utoronto.ca>
2016-11-28 05:42:50 -0500

    invicible

--- translateLinkSprite64x48.v
+++ translateLinkSprite64x48.v
@@ -1,4 +1,4 @@
-module translateLinkSprite64x48(input [5:0]x, input [5:0] y, output reg[11:0]mem_address);
+module translateLinkSprite64x64(input [5:0]x, input [5:0] y, output reg[11:0]mem_address);
 	always@(*)begin
 		mem_address = ({1'b0, y, 6'd0} + x);
 	end
